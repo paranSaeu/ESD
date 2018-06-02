aa8830a8b9fef980c1ae452f25dd14602e9e25c3
sju0924 <39671049+sju0924@users.noreply.github.com>
2018-06-03 00:53:02 +0900

    dd

--- ESDHateSpeechDictionary/app/src/main/java/kr/hs/gimpo/esdhatespeechdictionary/MainActivity.java
+++ ESDHateSpeechDictionary/app/src/main/java/kr/hs/gimpo/esdhatespeechdictionary/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.chaos);
+        textView.setText("살려줘...");
     }
 
 
