49c18cae9522295c38abfdbf488ca2efce8feae4
Users2530 <rhkgkrwk001@outlook.com>
2018-06-03 08:27:08 +0000

    Layout Improved.

--- ESDHateSpeechDictionary/app/src/main/java/kr/hs/gimpo/esdhatespeechdictionary/MainActivity.java
+++ ESDHateSpeechDictionary/app/src/main/java/kr/hs/gimpo/esdhatespeechdictionary/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
@@ -14,18 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView = (TextView) findViewById(R.id.chaos);
-        textView.setText("살려줘...");
-        Button button = (Button) findViewById(R.id.button2);
-        button.setOnClickListener(new View.OnClickListener() {
+        
+        Button search = (Button) findViewById(R.id.search);
+        
+        search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextView textView2 = (TextView) findViewById(R.id.error_text);
-                textView2.setText("please...");
+                EditText editText = (EditText) findViewById(R.id.editText);
+                TextView word = (TextView) findViewById(R.id.word_hint);
+                
+                word.setText(editText.getText());
+                editText.setText("");
             }
         });
     }
-
-
-
 }
