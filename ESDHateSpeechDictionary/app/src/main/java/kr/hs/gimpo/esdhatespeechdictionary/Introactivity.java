5238e05219a2571498127e110871ba38899d46b6
Users2530 <rhkgkrwk001@outlook.com>
2018-06-09 12:59:05 +0000

    Add Firebase DB

--- ESDHateSpeechDictionary/app/src/main/java/kr/hs/gimpo/esdhatespeechdictionary/Introactivity.java
+++ ESDHateSpeechDictionary/app/src/main/java/kr/hs/gimpo/esdhatespeechdictionary/Introactivity.java
@@ -4,12 +4,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Introactivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_introactivity);
+        
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(intent);
         finish();
--- ESDHateSpeechDictionary/app/src/main/java/kr/hs/gimpo/esdhatespeechdictionary/MainActivity.java
+++ ESDHateSpeechDictionary/app/src/main/java/kr/hs/gimpo/esdhatespeechdictionary/MainActivity.java
@@ -1,13 +1,14 @@
 package kr.hs.gimpo.esdhatespeechdictionary;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -16,24 +17,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView = (TextView) findViewById(R.id.chaos);
-        textView.setText("살려줘...");
-        Button button = (Button) findViewById(R.id.button2);
-        button.setOnClickListener(new View.OnClickListener() {
+    
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("ESD");
+        
+        mDatabase.child("test").setValue("Hello, World!");
+        
+        mDatabase.child("test").addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View v) {
-                TextView textView2 = (TextView) findViewById(R.id.error_text);
-                textView2.setText(R.string.hyunsoo);
-                EditText editText = (EditText) findViewById(R.id.writtingboard1);
-                String data = editText.getText().toString();
-                textView2.setText(data);
-                Intent intent = new Intent(getApplicationContext(), Introactivity.class);
-                startActivity(intent);
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                TextView textView = findViewById(R.id.textView);
+                
+                textView.setText(dataSnapshot.getValue(String.class));
             }
-
+    
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            
+            }
         });
+        
     }
-
-
-
 }
