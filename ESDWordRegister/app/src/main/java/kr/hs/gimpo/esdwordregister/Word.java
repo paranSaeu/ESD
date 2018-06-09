3e5b5edb38576cad702b2cd2d06b0f79292cde8f
Users2530 <rhkgkrwk001@outlook.com>
2018-06-09 03:08:27 +0000

    Release ESDWordRegister 2.0
    - New class added: Word
    - Word registration mechanism improved
    - Layout simplified
    - Application file updated!
    - Data structure changed
    
    - what's new in ESDWordRegister 2.0:
      - now you can register every words!
      - check word, and you can modify word data easily now
      - and more...

---
+++ ESDWordRegister/app/src/main/java/kr/hs/gimpo/esdwordregister/Word.java
@@ -0,0 +1,33 @@
+package kr.hs.gimpo.esdwordregister;
+
+public class Word {
+    
+    public Word() {
+    
+    }
+    
+    public String _Word, _Meaning, _Etymology, _Category;
+    
+    public Word(String _word, String _meaning, String _etymology, String _category) {
+        this._Word = _word;
+        this._Meaning = _meaning;
+        this._Etymology = _etymology;
+        this._Category = _category;
+    }
+    
+    public String get_Word() {
+        return this._Word;
+    }
+    
+    public String get_Meaning() {
+        return this._Meaning;
+    }
+    
+    public String get_Etymology() {
+        return this._Etymology;
+    }
+    
+    public String get_Category() {
+        return this._Category;
+    }
+}
